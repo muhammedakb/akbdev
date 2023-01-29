27e76a38665401b26953e35cb069c2aaf06a9be7
muhammedakb <muhammetakb68@gmail.com>
2023-01-29 17:42:54 +0300

    Embed React Components in our Markdown with MDX in a Next.js App

--- apps/site/pages/articles/[slug].tsx
+++ apps/site/pages/articles/[slug].tsx
@@ -1,30 +1,71 @@
+import {
+  getParsedFileContentBySlug,
+  markdown as renderMarkdown,
+} from '@akbdev/markdown';
+import { Youtube } from '@akbdev/shared/mdx-elements';
 import { readdirSync } from 'fs';
 import { GetStaticPaths, GetStaticProps } from 'next';
+import { MDXRemote, MDXRemoteProps } from 'next-mdx-remote';
 import { join } from 'path';
 import { ParsedUrlQuery } from 'querystring';
+
+type FrontMatter = {
+  title: string;
+  excerpt: string;
+  date: string;
+  author: {
+    name: string;
+  };
+};
 
 export interface ArticleProps extends ParsedUrlQuery {
   slug: string;
 }
 
+type Props = {
+  frontMatter: FrontMatter;
+  html: MDXRemoteProps;
+} & ArticleProps;
+
+const mdxElements = {
+  Youtube,
+};
+
 const POSTS_PATH = join(process.cwd(), '_articles');
 
-const Article = ({ slug }: ArticleProps) => {
+const Article = ({ frontMatter, html }: Props) => {
   return (
-    <div>
-      <h1>Visiting, {slug}</h1>
+    <div className="m-6">
+      <article className="prose prose-lg">
+        <h1>{frontMatter.title}</h1>
+        <div>
+          by {frontMatter.author.name} <span>{frontMatter.date}</span>
+        </div>
+      </article>
+      <hr />
+      <MDXRemote {...html} components={mdxElements} />
     </div>
   );
 };
 
-export const getStaticProps: GetStaticProps<ArticleProps> = async ({
+export const getStaticProps: GetStaticProps<Props> = async ({
   params,
 }: {
   params: ArticleProps;
 }) => {
+  // 1. parse the content of our markdown and separate it into frontmatter and content
+  const articleMarkdownContent = getParsedFileContentBySlug(
+    params.slug,
+    POSTS_PATH
+  );
+
+  // 2. convert markdown content => HTML
+  const renderHTML = await renderMarkdown(articleMarkdownContent.content);
+
   return {
     props: {
-      slug: params.slug,
+      frontMatter: articleMarkdownContent.frontMatter,
+      html: renderHTML,
     },
   };
 };
@@ -35,18 +76,6 @@ export const getStaticPaths: GetStaticPaths<ArticleProps> = async () => {
     .map((slug) => ({ params: { slug } }));
   // http://localhost:4200/articles/dynamic-routing
   return {
-    // paths: [
-    //   {
-    //     params: {
-    //       slug: 'page1',
-    //     },
-    //   },
-    //   {
-    //     params: {
-    //       slug: 'page2',
-    //     },
-    //   },
-    // ],
     paths,
     fallback: false,
   };
